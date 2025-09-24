592a909f576288bd04528d9b03145874aedb8602
Nidhi raj <nidhiraj26031998@gmail.com>
2025-09-24 15:10:56 +0530

    basic frontend and backend setup

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+const port = 2620;
+
+// TO PUT AlL ENTERED VALUES IN BODY KEY
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/test", (req, res) => {
+  res.json({
+    message: "Hello! Server is running 🚀, done by nidhi raj",
+    status: "success",
+  });
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log("Firing up the express server on: ", port);
+});
